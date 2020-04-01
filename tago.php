bd3e12ca99274c94400d67047afd47fd664dd502
dimasAgung99 <dimaz.agunk99@gmail.com>
2020-04-01 19:54:09 +0700

    update tago.php

--- tago.php
+++ tago.php
@@ -5,17 +5,27 @@ error_reporting(1);
 
 system ("clear");
 
-$accessToken = "SfTGNKCJdKnvtrLGsKNitx33";
+function config()
+{
+  
+  $config = [];
+  $config["accessToken"] = "SfTGNKCJdKnvtrLGsKNitx33";
+  $config["userId"] = "410158";
+  $config["aid"] = "a0e82c1536512fd3";
+  $config["token"] = "ecp0MuaTEmg%3AAPA91bEGesFIMzHmael08Ve3S8SIgKCXL0C0tssBLD0EIEmrUnPde1a5ERXl6UjWf7-Uoae8Y4srV-xWe3csn0M1juuPCjhu6dyzu6SD7iyDUYnjtULkEonO27RyA-Dw6CGLIPvMW5Be";
+  $config['userAgent'] = "Dalvik/2.1.0 (Linux; U; Android 5.1.1; Redmi 3 MIUI/V7.2.1.0.LHPMIDB)";
+  
+  return $config;
+  
+}
 
-$userId = "410158";
 
-$aid = "a0e82c1536512fd3";
-
-$token = "ecp0MuaTEmg%3AAPA91bEGesFIMzHmael08Ve3S8SIgKCXL0C0tssBLD0EIEmrUnPde1a5ERXl6UjWf7-Uoae8Y4srV-xWe3csn0M1juuPCjhu6dyzu6SD7iyDUYnjtULkEonO27RyA-Dw6CGLIPvMW5Be";
 
 $green = "\e[92m";
 
 $orange = "\e[38;5;208m";
+
+$yellow = "\e[93m";
 
 $white = "\e[97m";
 
@@ -30,7 +40,7 @@ echo "$bold $orange
                          
 $white";
 
-echo "Creator : Dimas Agung Hidayat\n\n\n";
+echo "creator : dimasAgung\n\n\n";
 
 
 function requestPost($url,$headers,$body)
@@ -90,22 +100,21 @@ function userInit()
 {
   
   
-  global $accessToken,$userId,$aid,$token;
+  $config = config();
   
-  $url = "https://tago.games/prod/api/user/init?access_token={$accessToken}&user_id={$userId}&aid={$aid}&token=".$token;
+  $url = "https://tago.games/prod/api/user/init?access_token={$config['accessToken']}&user_id={$config['userId']}&aid={$config['aid']}&token=".$config['token'];
   
   $headerRaw = "Accept-Language: in-ID,in;q=0.8
-User-Agent: Dalvik/2.1.0 (Linux; U; Android 5.1.1; Redmi 3 MIUI/V7.2.1.0.LHPMIDB)
+User-Agent: {$config['userAgent']}
 Authorization: Basic dGFnb19pY2FuOkQnMyouQC00SG5yQEx7fi8=
 Host: tago.games
-Connection: Keep-Alive
-Accept-Encoding: gzip";
+Connection: Keep-Alive";
 
   $headers = headers($headerRaw);
   
   $result = requestGet($url,$headers);
   
-  $array = json_decode(gzdecode($result),true);
+  $array = json_decode($result,true);
   
   
   
@@ -119,11 +128,73 @@ Accept-Encoding: gzip";
   
 }
 
+function userDashboard()
+{
+  
+  $config = config();
+  
+  $url = "https://tago.games/prod/api/user/dashboard?access_token={$config['accessToken']}&user_id=".$config['userId'];
+  
+  $headerRaw = "Accept-Language: in-ID,in;q=0.8
+User-Agent: {$config['userAgent']}
+Host: tago.games
+Connection: Keep-Alive";
+  
+  $headers = headers($headerRaw);
+  
+  $result = requestGet($url,$headers);
+  
+  //echo $result;
+  
+}
+
+function userDailyBonus()
+{
+  global $green,$white,$yellow;
+  
+  $config = config();
+  
+  $url = "https://tago.games/prod/api/user/daily_bonus";
+  
+  $body = "access_token={$config['accessToken']}&user_id=".$config['userId'];
+  
+  $headerRaw = "Accept-Language: in-ID,in;q=0.8
+User-Agent: {$config['userAgent']}
+Content-Type: application/x-www-form-urlencoded
+Host: tago.games
+Connection: Keep-Alive";
+
+  $headers = headers($headerRaw);
+  
+  $result = requestPost($url,$headers,$body);
+  
+  $array = json_decode($result,true);
+  
+  if($array['status'] == 'fail')
+  {
+    
+    extract($array['data']);
+    
+    echo "{$green}•{$white} Daily Bonus ~{$green} Day ".$day."{$white} ->{$yellow} Besok\n\n";
+    
+  }
+  else 
+  {
+    
+    extract($array['data']);
+    
+    echo "{$green}•{$white} Daily Bonus ~{$yellow} Day ".$day."{$white} ->{$green} ".$array['message']."\n\n";
+    
+  }
+  
+}
 
 function userCoinEarn()
 {
   
-  global $accessToken,$userId,$white,$green;
+  global $white,$green,$yellow;
+  
+  $config = config();
   
   $url = [];
   
@@ -133,12 +204,12 @@ function userCoinEarn()
   
   $body = [];
   
-  $body[0] = "access_token={$accessToken}&user_id={$userId}";
+  $body[0] = "access_token={$config['accessToken']}&user_id=".$config['userId'];
   
-  $body[1] = "access_token={$accessToken}&user_id={$userId}&type=X2_TAGO";
+  $body[1] = $body[0]."&type=X2_TAGO";
   
   $headerRaw = "Accept-Language: in-ID,in;q=0.8
-User-Agent: Dalvik/2.1.0 (Linux; U; Android 5.1.1; Redmi 3 MIUI/V7.2.1.0.LHPMIDB)
+User-Agent: {$config['userAgent']}
 Content-Type: application/x-www-form-urlencoded
 Host: tago.games
 Connection: Keep-Alive";
@@ -161,21 +232,32 @@ Connection: Keep-Alive";
       if($resultArray["status"] == "success")
       {
       	
-      	   
-      	echo "• {$white}collect ->{$green} ".$resultArray["status"]."\n";
+      	if($i == 0)
+      	{
+      	  
+      	  echo "{$green}• {$white}collect tago bonus ->{$green} ".$resultArray["status"]."\n";
+      	  
+      	}
+      	else 
+      	{
+      	  
+      	  echo "{$green}• {$white}collect tago bonus x2 ->{$green} ".$resultArray["status"]."\n";
+      	  
+      	}
+      	
       	  
       }
       else
       {
          
-         echo $resultArray["message"]."\n";
+         echo "{$white}•{$yellow} ".$resultArray["message"]."\n";
          $kondisi = false;
          break;
          
       }
       
       
-      sleep(5);
+      sleep(30);
     
   
     }
@@ -189,21 +271,22 @@ Connection: Keep-Alive";
 function userConvert()
 {
   
-  global $accessToken,$userId,$green,$white;
+  global $green,$white;
   
-  $url = "https://tago.games/prod/api/user/convert?access_token={$accessToken}&user_id=".$userId;
+  $config = config();
+  
+  $url = "https://tago.games/prod/api/user/convert?access_token={$config['accessToken']}&user_id=".$config['userId'];
   
   $headerRaw = "Accept-Language: in-ID,in;q=0.8
-User-Agent: Dalvik/2.1.0 (Linux; U; Android 5.1.1; Redmi 3 MIUI/V7.2.1.0.LHPMIDB)
+User-Agent: {$config['userAgent']}
 Host: tago.games
-Connection: Keep-Alive
-Accept-Encoding: gzip";
+Connection: Keep-Alive";
   
   $headers = headers($headerRaw);
   
   $result = requestGet($url,$headers);
   
-  $array = json_decode(gzdecode($result),true);
+  $array = json_decode($result,true);
   
   if($array["status"] == "success")
   {
@@ -241,26 +324,29 @@ Accept-Encoding: gzip";
 function userGames()
 {
   
-  global $accessToken,$userId;
+  $config = config();
   
-  $url = "https://tago.games/prod/api/user/games?access_token={$accessToken}&user_id=".$userId;
+  $url = "https://tago.games/prod/api/user/games?access_token={$config['accessToken']}&user_id=".$config['userId'];
   
   $headerRaw = "Accept-Language: in-ID,in;q=0.8
-User-Agent: Dalvik/2.1.0 (Linux; U; Android 5.1.1; Redmi 3 MIUI/V7.2.1.0.LHPMIDB)
+User-Agent: {$config['userAgent']}
 Host: tago.games
-Connection: Keep-Alive
-Accept-Encoding: gzip";
+Connection: Keep-Alive";
 
   $headers = headers($headerRaw);
   
   $result = requestGet($url,$headers);
   
-  $array = json_decode(gzdecode($result),true);
+  $array = json_decode($result,true);
   
   if($array["status"] == "success")
   {
     
-    return $array["data"][0];
+    $dataLength = count ($array["data"]);
+    
+    $randomId = rand(0,$dataLength);
+    //return $array["data"][0];
+    return $array["data"][$randomId];
     
   }
   
@@ -269,7 +355,9 @@ Accept-Encoding: gzip";
 function startGame()
 {
   
-  global $accessToken,$userId,$green,$white;
+  global $green,$white;
+  
+  $config = config();
   
   echo "\n\n";
   
@@ -289,15 +377,14 @@ function startGame()
     $url = "https://tago.games/prod/api/game/mulai";
   
     $headerRaw = "Accept-Language: in-ID,in;q=0.8
-  User-Agent: Dalvik/2.1.0 (Linux; U; Android 5.1.1; Redmi 3 MIUI/V7.2.1.0.LHPMIDB)
+  User-Agent: {$config['userAgent']}
   Content-Type: application/x-www-form-urlencoded
   Host: tago.games
-  Connection: Keep-Alive
-  Accept-Encoding: gzip";
+  Connection: Keep-Alive";
   
     $headers = headers($headerRaw);
     
-    $body = "access_token={$accessToken}&user_id={$userId}&game_id=".$gameId;
+    $body = "access_token={$config['accessToken']}&user_id={$config['userId']}&game_id=".$gameId;
     
     $result = requestPost($url,$headers,$body);
     
@@ -308,7 +395,7 @@ function startGame()
       
       $idStartGame = $array["data"]["id"];
       
-      echo "{$white}".$i.".{$green} Play Game{$white} ".$nameGame."\n\n";
+      echo "{$white}".$i.".{$green} Play Game{$white} ".$nameGame."\n";
       
       $minutes = $minTime / 60;
       
@@ -357,24 +444,25 @@ function startGame()
 function EndGame($idStartGame)
 {
   
-  global $accessToken,$userId,$green,$white;
+  global $green,$white;
+  
+  $config = config();
   
   $url = "https://tago.games/prod/api/game/akhir";
   
   $headerRaw = "Accept-Language: in-ID,in;q=0.8
-User-Agent: Dalvik/2.1.0 (Linux; U; Android 5.1.1; Redmi 3 MIUI/V7.2.1.0.LHPMIDB)
+User-Agent: {$config['userAgent']}
 Content-Type: application/x-www-form-urlencoded
 Host: tago.games
-Connection: Keep-Alive
-Accept-Encoding: gzip";
+Connection: Keep-Alive";
   
   $headers = headers($headerRaw);
   
-  $body = "access_token={$accessToken}&user_id={$userId}&id={$idStartGame}&contare=".rand(50,100);
+  $body = "access_token={$config['accessToken']}&user_id={$config['userId']}&id={$idStartGame}&contare=".rand(50,100);
   
   $result = requestPost($url,$headers,$body);
   
-  $array = json_decode(gzdecode($result),true);
+  $array = json_decode($result,true);
   
   if($array["status"] == "success")
   {
@@ -382,8 +470,9 @@ Accept-Encoding: gzip";
     //return $result;
   
     
-    return "{$green}   ••{$white} Reward + ".$array["data"]["reward"]." {$green}••\n\n";
-    
+    return "{$green}   ~{$white} Reward +{$GLOBALS['yellow']} ".$array["data"]["reward"]." {$green}~\n";
+    /*
+    return "Reward + ".$array["data"]["reward"]."\n";*/
   
   }
   else
@@ -396,9 +485,14 @@ Accept-Encoding: gzip";
 }
 
 
+
 userInit();
 
+userDashboard();
+
 userConvert();
+
+userDailyBonus();
 
 userCoinEarn();
 
