3c5d00753f7bb4531c891a415e3c4e67b1e53933
dimasAgung99 <dimaz.agunk99@gmail.com>
2020-04-06 15:47:07 +0700

    update

--- tago.php
+++ tago.php
@@ -9,10 +9,10 @@ function config()
 {
   
   $config = [];
-  $config["accessToken"] = "SfTGNKCJdKnvtrLGsKNitx33";
+  $config["accessToken"] = "iHC8ReAGeY5JM9KOtERhiufE";
   $config["userId"] = "410158";
   $config["aid"] = "a0e82c1536512fd3";
-  $config["token"] = "ecp0MuaTEmg%3AAPA91bEGesFIMzHmael08Ve3S8SIgKCXL0C0tssBLD0EIEmrUnPde1a5ERXl6UjWf7-Uoae8Y4srV-xWe3csn0M1juuPCjhu6dyzu6SD7iyDUYnjtULkEonO27RyA-Dw6CGLIPvMW5Be";
+  $config["token"] = "ecp0MuaTEmg:APA91bEGesFIMzHmael08Ve3S8SIgKCXL0C0tssBLD0EIEmrUnPde1a5ERXl6UjWf7-Uoae8Y4srV-xWe3csn0M1juuPCjhu6dyzu6SD7iyDUYnjtULkEonO27RyA-Dw6CGLIPvMW5Be";
   $config['userAgent'] = "Dalvik/2.1.0 (Linux; U; Android 5.1.1; Redmi 3 MIUI/V7.2.1.0.LHPMIDB)";
   
   return $config;
